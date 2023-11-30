5a96e759597bbd5b785deed1fe91b243645a4bca
AnnaFeona <glavatskikh.anna@gmail.com>
2023-12-01 01:23:30 +0300

    config: install react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import './App.css';
+import './App.scss';
+// import { useForm } from 'react-hook-form';
 
 function App() {
+  // const { register, handleSubmit } = useForm();
   return (
     <>
       <div>App works</div>
